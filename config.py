54a9672108e7a193ed0d7e7c044be9c285d1e7fb
rakesh2026cse <rakeshkumarsahoo2026cse@gmail.com>
2025-04-14 11:58:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
-# config.py
 
 DB_PATH = "data/database.db"
-SENSOR_INTERVAL = 5  # seconds between sensor reads
+SENSOR_INTERVAL = 5 
 
-# Thresholds (can be learned by ML too)
-TEMP_LIMIT = 80        # °C
-VIBRATION_LIMIT = 5.0  # arbitrary units
-PRESSURE_LIMIT = 150   # PSI
+TEMP_LIMIT = 80      
+VIBRATION_LIMIT = 5.0  
+PRESSURE_LIMIT = 150  
